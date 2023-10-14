537134c95624107cbd508132b26a0bcc3f4522ef
“LojainHamza” <lojainehab17@gmail.com>
2023-10-14 12:01:22 -0700

    leo

--- alias.c
+++ alias.c
@@ -11,37 +11,37 @@
 
 int addNewAlias(alias **head, char *key, char *value)
 {
-    alias *node, *_head;
+	alias *node, *_head;
 
-    if (!head)
-        return -1;
+	if (!head)
+	return (-1);
 
-    node = _malloc(sizeof(alias));
-    node->key = _strdup(key);
-    node->value = _strdup(value);
-    node->next = NULL;
+	node = _malloc(sizeof(alias));
+	node->key = _strdup(key);
+	node->value = _strdup(value);
+	node->next = NULL;
 
-    if (!*head)
-    {
-        *head = node;
-        return 0;
-    }
+	if (!*head)
+	{
+	*head = node;
+	return (0);
+	}
 
-    _head = *head;
-    while (_head->next)
-    {
-        if (!_strcmp(_head->key, key))
-        {
-            free(_head->value);
-            _head->value = _strdup(value);
-            freealias(node);
-            return 0;
-        }
-        _head = _head->next;
-    }
+	_head = *head;
+	while (_head->next)
+	{
+	if (!_strcmp(_head->key, key))
+	{
+	free(_head->value);
+	_head->value = _strdup(value);
+	freealias(node);
+	return (0);
+	}
+	_head = _head->next;
+	}
 
-    _head->next = node;
-    return 0;
+	_head->next = node;
+	return (0);
 }
 
 /**
@@ -52,20 +52,20 @@ int addNewAlias(alias **head, char *key, char *value)
 
 int printAllAliases(alias *head)
 {
-    if (!head)
-        return -1;
+	if (!head)
+	return (-1);
 
-    while (head)
-    {
-        _write(-1, NULL, 0);
-        _write(1, head->key, _strlen(head->key));
-        _write(1, "='", 2);
-        _write(1, head->value, _strlen(head->value));
-        _write(1, "'\n", 2);
-        _write(1, NULL, 0);
-        head = head->next;
-    }
-    return 0;
+	while (head)
+	{
+	_write(-1, NULL, 0);
+	_write(1, head->key, _strlen(head->key));
+	_write(1, "='", 2);
+	_write(1, head->value, _strlen(head->value));
+	_write(1, "'\n", 2);
+	_write(1, NULL, 0);
+	head = head->next;
+	}
+	return (0);
 }
 
 /**
@@ -77,47 +77,47 @@ int printAllAliases(alias *head)
  */
 int printAliasByKey(alias *head, char *key)
 {
-    char *msg, *smn;
+	char *msg, *smn;
 
-    if (!head)
-    {
-        errno = -5;
-        msg = _malloc(_strlen("not found ") + _strlen(key) + 4);
-        _strcpy(msg, "not found ");
-        smn = _malloc(_strlen("alias: ") + _strlen(key) + 4);
-        _strcpy(smn, "alias: ");
-        _strcat(smn, key);
-        print_error(smn, NULL, msg);
-        free(msg);
-        free(smn);
-        return -1;
-    }
+	if (!head)
+	{
+	errno = -5;
+	msg = _malloc(_strlen("not found ") + _strlen(key) + 4);
+	_strcpy(msg, "not found ");
+	smn = _malloc(_strlen("alias: ") + _strlen(key) + 4);
+	_strcpy(smn, "alias: ");
+	_strcat(smn, key);
+	print_error(smn, NULL, msg);
+	free(msg);
+	free(smn);
+	return (-1);
+	}
 
-    while (head)
-    {
-        if (!_strcmp(head->value, key))
-        {
-            _write(-1, NULL, 0);
-            _write(1, head->key, _strlen(head->key));
-            _write(1, "='", 2);
-            _write(1, head->value, _strlen(head->value));
-            _write(1, "'\n", 2);
-            _write(1, NULL, 0);
-            return 0;
-        }
-        head = head->next;
-    }
+	while (head)
+	{
+	if (!_strcmp(head->value, key))
+	{
+	_write(-1, NULL, 0);
+	_write(1, head->key, _strlen(head->key));
+	_write(1, "='", 2);
+	_write(1, head->value, _strlen(head->value));
+	_write(1, "'\n", 2);
+	_write(1, NULL, 0);
+	return (0);
+	}
+	head = head->next;
+	}
 
-    errno = -3;
-    msg = _malloc(_strlen("not found ") + _strlen(key) + 4);
-    _strcpy(msg, "not found ");
-    smn = _malloc(_strlen("alias: ") + _strlen(key) + 4);
-    _strcpy(smn, "alias: ");
-    _strcat(smn, key);
-    print_error(smn, NULL, msg);
-    free(msg);
-    free(smn);
-    return -1;
+	errno = -3;
+	msg = _malloc(_strlen("not found ") + _strlen(key) + 4);
+	_strcpy(msg, "not found ");
+	smn = _malloc(_strlen("alias: ") + _strlen(key) + 4);
+	_strcpy(smn, "alias: ");
+	_strcat(smn, key);
+	print_error(smn, NULL, msg);
+	free(msg);
+	free(smn);
+	return (-1);
 }
 
 /**
@@ -130,29 +130,29 @@ int printAliasByKey(alias *head, char *key)
 
 int handleAliases(char **arg, alias **aliashead)
 {
-    int argc = _arlen(arg);
-    int i = 0;
-    char **tmp = NULL;
+	int argc = _arlen(arg);
+	int i = 0;
+	char **tmp = NULL;
 
-    if (argc == 0)
-        return -1;
+	if (argc == 0)
+	return (-1);
 
-    if (argc == 1)
-        printAllAliases(*aliashead);
+	if (argc == 1)
+	printAllAliases(*aliashead);
 
-    if (argc > 1)
-    {
-        i += 1;
-        while (arg[i])
-        {
-            parse_args(arg[i], "=", &tmp, 0);
-            if (_arlen(tmp) > 1)
-                addNewAlias(aliashead, tmp[0], tmp[1]);
-            else
-                printAliasByKey(*aliashead, tmp[0]);
-            free_pp(tmp);
-            i++;
-        }
-    }
-    return 0;
+	if (argc > 1)
+	{
+	i += 1;
+	while (arg[i])
+	{
+	parse_args(arg[i], "=", &tmp, 0);
+		if (_arlen(tmp) > 1)
+		addNewAlias(aliashead, tmp[0], tmp[1]);
+		else
+		printAliasByKey(*aliashead, tmp[0]);
+		free_pp(tmp);
+		i++;
+	}
+	}
+	return (0);
 }
