56f4d1abec420b1f3e8e8de60f2ddf24b3674913
“LojainHamza” <lojainehab17@gmail.com>
2023-10-14 11:41:38 -0700

    leo

--- str_token.c
+++ str_token.c
@@ -10,44 +10,44 @@
  */
 char *tokenizeString(char *str, const char *delimiter, int whichf)
 {
-    static char *save;
-    char *_new = NULL;
-    int i = 0, (*func)(char *, const char *), loc, in_quotes = 0;
+	static char *save;
+	char *_new = NULL;
+	int i = 0, (*func)(char *, const char *), loc, in_quotes = 0;
 
-    func = (whichf == 0) ? _strcmpd : _strcmps;
-    loc = (whichf) ? _strlen(delimiter) - 1 : 0;
-    if (!str || !*str)
-    {
-        if (!save || !*save)
-            return NULL;
-        while (in_quotes || (func(save + i, delimiter) != 1 && *(save + i) != '\0'))
-        {
-            if (*(save + i) == '\'' || *(save + i) == '\"')
-                in_quotes = !in_quotes;
-            i++;
-        }
-        if (*(save + i) == '\0')
-        {
-            _new = save;
-            save = NULL;
-            return _new;
-        }
-        _new = save;
-        *(save + i) = '\0';
-        save = save + i + loc + 1;
-        return _new;
-    }
-    while (in_quotes || (func(str + i, delimiter) != 1 && *(str + i) != '\0'))
-    {
-        if (*(str + i) == '\'' || *(str + i) == '\"')
-            in_quotes = !in_quotes, i++;
-    }
-    if (*(str + i) == '\0')
-    {
-        save = NULL;
-        return str;
-    }
-    save = str + i + loc + 1;
-    *(str + i) = '\0';
-    return str;
+	func = (whichf == 0) ? _strcmpd : _strcmps;
+	loc = (whichf) ? _strlen(delimiter) - 1 : 0;
+	if (!str || !*str)
+	{
+	if (!save || !*save)
+	return (NULL);
+	while (in_quotes || (func(save + i, delimiter) != 1 && *(save + i) != '\0'))
+	{
+	if (*(save + i) == '\'' || *(save + i) == '\"')
+		in_quotes = !in_quotes;
+		i++;
+	}
+	if (*(save + i) == '\0')
+	{
+	_new = save;
+	save = NULL;
+	return (_new);
+	}
+	_new = save;
+	*(save + i) = '\0';
+	save = save + i + loc + 1;
+	return (_new);
+	}
+	while (in_quotes || (func(str + i, delimiter) != 1 && *(str + i) != '\0'))
+	{
+	if (*(str + i) == '\'' || *(str + i) == '\"')
+		in_quotes = !in_quotes, i++;
+	}
+	if (*(str + i) == '\0')
+	{
+	save = NULL;
+	return (str);
+	}
+	save = str + i + loc + 1;
+	*(str + i) = '\0';
+	return (str);
 }
