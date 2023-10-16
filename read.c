e539513d9f3c975893c72d17890dc25a971dfd0f
“LojainHamza” <lojainehab17@gmail.com>
2023-10-16 11:40:32 -0700

    leo

--- read.c
+++ read.c
@@ -9,24 +9,25 @@
  *
  * Return: length of input
  */
-int read_user_input(char **input, size_t *input_len, char ***commands, int file_descriptor)
+int read_user_input(char **input,
+		size_t *input_len, char ***commands, int file_descriptor)
 {
-    int length;
+	int length;
 
-    if (isatty(0) && !file_descriptor)
-        write(1, "$ ", 2);
+	if (isatty(0) && !file_descriptor)
+	write(1, "$ ", 2);
 
-    length = _getline(input, input_len, file_descriptor);
+	length = _getline(input, input_len, file_descriptor);
 
-    if (length == -1)
-    {
-        free(*input);
-        write(1, "\n", 1);
-        exit(0);
-    }
+	if (length == -1)
+	{
+	free(*input);
+	write(1, "\n", 1);
+	exit(0);
+	}
 
-    (*input)[length - 1] = '\0';
-    parse_arguments(*input, ";", commands, 0);
+	(*input)[length - 1] = '\0';
+	parse_arguments(*input, ";", commands, 0);
 
-    return (length - 1);
+	return (length - 1);
 }
