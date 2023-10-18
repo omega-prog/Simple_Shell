8eaea727cc0825c8bda56a1ba43f37e10c413cab
omega-prog <70572914+omega-prog@users.noreply.github.com>
2023-10-19 02:20:19 +0300

    Update file.desc.c

--- file.desc.c
+++ file.desc.c
@@ -1,54 +1,58 @@
 #include "shell.h"
 
 /**
- * writeToDescriptor - write to file descriptor
+ * customWrite - write to file descriptor
  * @fd: file descriptor
- * @inp: string to write
- * @len: number of characters in the string
+ * @inputString: string to write
+ * @length: number of characters in the string
  *
- * Return: -100 when no queue, 100 when successfully write to
+ * Return: -100 when no queue, 100 when successfully written, 0 when no input
  */
 
-int writeToDescriptor(int fd, char *inp, int len)
+int customWrite(int fd, char *inputString, int length)
 {
-	static char writeQueue[BUFFER_t];
-	static int queueSize;
-	int i = 0;
+    static char writeBuffer[BUFFER_t];
+    static int queueLength;
+    int i;
 
-	if (fd < 0)
-	{
-	queueSize = 0;
-	return (-100);
-	}
+    if (fd < 0)
+    {
+        queueLength = 0;
+        return (-100);
+    }
 
-	if (inp)
-	{
-	if ((len + queueSize) >= BUFFER_t)
-	{
-		if (write(fd, writeQueue, BUFFER_t) == -1)
-		{
-		print_error(NULL, NULL, "problem writing");
-		exit(-1);
-		}
-		queueSize = 0;
-	}
-	while (i < len)
-	{
-		writeQueue[queueSize + i] = inp[i], i++;
-	}
+    if (inputString)
+    {
+        if ((length + queueLength) >= BUFFER_t)
+        {
+            if (write(fd, writeBuffer, BUFFER_t) == -1)
+            {
+                print_error(NULL, NULL, "problem writing");
+                exit(-1);
+            }
+            queueLength = 0;
+        }
 
-	writeQueue[queueSize + i] = '\0';
-	queueSize += len;
-	return (100);
-	}
+        i = 0;
+        while (i < length)
+        {
+            writeBuffer[queueLength + i] = inputString[i];
+            i++;
+        }
 
-	if (!inp)
-	{
-	if (write(fd, writeQueue, queueSize) == -1)
-	{
-		print_error(NULL, NULL, "problem writing"), exit(-1);
-	}
-	}
+        writeBuffer[queueLength + i] = '\0';
+        queueLength += length;
+        return (100);
+    }
 
-	return (0);
+    if (!inputString)
+    {
+        if (write(fd, writeBuffer, queueLength) == -1)
+        {
+            print_error(NULL, NULL, "problem writing");
+            exit(-1);
+        }
+    }
+
+    return 0;
 }
