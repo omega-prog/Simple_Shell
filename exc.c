be762f650006bcbc5872d09e3b38b724b314ffa0
LojainHamza <136014339+LojainHamza@users.noreply.github.com>
2023-10-18 23:51:01 +0300

    Update exc.c

--- exc.c
+++ exc.c
@@ -14,7 +14,7 @@ int custom_execute(char **args)
 	if (!args)
 	return (exit_status);
 
-	file_path = get_custom_file_path(args[0]);
+	file_path = find_executable_path(args[0]);
 
 	if (stat(file_path, &cmd_info) == 0 && cmd_info.st_mode & S_IXUSR)
 	exit_status = execve(file_path, args, environ);
