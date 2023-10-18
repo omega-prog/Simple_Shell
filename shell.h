7f7ddd0462429543930814ed2674d69af25f5c27
omega-prog <70572914+omega-prog@users.noreply.github.com>
2023-10-19 01:47:58 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -2,7 +2,6 @@
 #define SIMPLE_SHELL
 
 #include <stdlib.h>
-#include <ctype.h>
 
 #include <sys/wait.h>
 #include <sys/types.h>
@@ -13,81 +12,111 @@
 #include <errno.h>
 #include <fcntl.h>
 
-#define BUFFER_t 1024
+#define BUFFER_SIZE 1024
 
 /**
  * struct cmdnode - a node for one command in a command list
  * @cmd: command string
- * @op: operation between current command and next command
- * @estat: exit status (shows if the command has been excuted or not)
+ * @op: operation between the current command and the next command
+ * @estat: exit status (shows if the command has been executed or not)
  * @next: a pointer to the next operand
  */
 
 typedef struct cmdnode
 {
-	char *cmd;
-	char *op;
-	int estat;
-	struct cmdnode *next;
+    char *cmd;
+    char *op;
+    int estat;
+    struct cmdnode *next;
 } cmdnode;
 
 /**
  * struct alias - a node for one alias
- * @key: the alais
+ * @key: the alias
  * @value: value of the alias
  * @next: a pointer to the next alias
  */
 
 typedef struct alias
 {
-	char *key;
-	char *value;
-	struct alias *next;
+    char *key;
+    char *value;
+    struct alias *next;
 } alias;
 
+void moveCommand(cmdnode **, int);
+int executeCommand(char **, int, alias **);
 
-int convertStringToInt(const char *str);
-int customGetline(char **lnptr, size_t *size, int fd);
-char *numberToString(int n);
-char *concatenateStrings(char *dest, char *src);
-int alternateStringComparison(char *fstring, const char *sub);
-int customStringComparison(char *fstring, const char *sub);
-int compareStrings(char *s1, char *s2);
-char *copyString(char *dest, const char *src);
-char *duplicateString(char *str);
-int getStringLength(const char *str);
-char *tokenizeString(char *str, const char *delimiter, int whichf);
-int writeToDescriptor(int fd, char *inp, int len);
-int printAliasByKey(alias *head, char *key);
-int handleAliases(char **arg, alias **aliashead);
-int addNewAlias(alias **head, char *key, char *value);
-int change_current_directory(char *path);
-int custom_putenv(char *es);
-int custom_setenv(cch *name, cch *value, __attribute__((unused))int overwrite);
-int custom_unsetenv(const char *name);
-void custom_printenv(void);
-char *custom_getenv(const char *name);
-void custom_print_error(char *prog, int *cmdr, char *msg);
-char *find_executable_path(char *command_name);
-int display_command_history(void);
-int save_command_history(char *command);
-int read_user_input(char **input, size_t *input_len, char ***commands, int file_descriptor);
-void free_string_array(char **array);
-void remove_extra_spaces(char **str, char *original);
-void *allocate_memory(unsigned int size);
-void *reallocate_memory(void *ptr, unsigned int size);
-int add_command_node(command_node **head, char *cmd, char *op);
-command_node *build_command_list(char *cmds);
-int print_command_nodes(command_node *head);
-void free_command_list(command_node *head);
-command_node *add_command_node_at_index(command_node **head, char *cmd, char *operator, int pos);
-void tokenize_and_parse(char *input, const char *delimiter, char ***parsed_args, int mode);
-int calculate_exponent(unsigned int base, int exponent);
-int execute_shell_script(char *script_path);
-int replace_and_count_occurrences(char **str, char *to_replace, char *replace_with);
-int *handle_builtin_commands(char **cmd, alias **aliases);
-int freealias(alias *head);
-int get_string_array_length(char **array);
+char *getFilePath(char *);
+void parseArguments(char *, const char *, char ***, int);
+int getInput(char **, size_t *, char ***, int);
+void trimSpaces(char **, char *);
+
+int *handleBinary(char **, alias **);
+
+void freePointerArray(char **);
+int arrayLength(char **);
+void *customMalloc(unsigned int);
+void *customRealloc(void *, unsigned int);
+
+char **arrayDuplicate(char **);
+void printDebug(char **);
+
+int customGetLine(char **, size_t *, int);
+int customStringCompareDelim(char *, const char *);
+char *customStrtok(char *, const char *, int);
+int customStringCompare(char *, const char *);
+
+int findTokenOccurrences(char *, char *);
+int findAndReplace(char **, char *, char *);
+
+int customStrlen(const char *);
+char *customStrcat(char *, char *);
+char *customStrdup(char *);
+int customStringCompare(char *, char *);
+char *customStrcpy(char *, char *);
+
+int customPutenv(char *);
+int customSetenv(const char *, const char *, int);
+int customUnsetenv(const char *);
+char *customGetenv(const char *);
+void printEnvironment(void);
+
+int customChdir(char *);
+int runScript(char *);
+int executeCommand(char **);
+
+int expandString(char **, int);
+
+int addAlias(alias **, char *, char *);
+int printLongAlias(alias *);
+int printAlias(alias *, char *);
+int handleAlias(char **, alias **);
+int freeAlias(alias *);
+
+void expandAlias(char **, alias *);
+
+int removeAlias(alias **, char *);
+
+int addCommandNode(cmdnode **, char *, char *);
+cmdnode *buildCommandList(char *);
+int printCommandNodes(cmdnode *);
+void freeCommandList(cmdnode *);
+cmdnode *addCommandNodeAtIndex(cmdnode **, char *, char *, int);
+
+int powerOf(int base, int power);
+int numberLength(int num);
+char *intToString(int);
+int customAtoi(char *);
+
+void printHelp(char *arg);
+
+int printCommandHistory(void);
+int writeCommandHistory(char *);
+
+int customWrite(int, char *, int);
+
+void printErrorMessage(char *, int *, char *);
 
 extern char **environ;
 
