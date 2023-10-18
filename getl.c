9ff24040fcdc773b2c139c1c5ab72c3d521f1969
omega-prog <70572914+omega-prog@users.noreply.github.com>
2023-10-19 01:49:50 +0300

    Update getl.c

--- getl.c
+++ getl.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * customGetline - get line of string from file
+ * customGetLine - get line of string from file
  * @lnptr: variable to store string
  * @size: number of strings stored
  * @fd: file to read from
@@ -9,46 +9,40 @@
  * Return: the length of lnptr or -1 on failure
  */
 
-int customGetline(char **lnptr, size_t *size, int fd)
+int customGetLine(char **lnptr, size_t *size, int fd)
 {
-	char buffer[BUFFER_t + 1];
-	int r = BUFFER_t, len = 0, mode = isatty(0);
-	char *tmp;
+    char buffer[BUFFER_SIZE + 1];
+    int r = BUFFER_SIZE, len = 0, mode = isatty(0);
+    char *tmp;
 
-	*lnptr = NULL;
-	while (r == BUFFER_t)
-	{
-	if (*lnptr && len > 0)
-		if ((*lnptr)[len - 1] == '\n' && mode)
-		break;
-	r = read(fd, buffer, BUFFER_t);
-	if (r < 0 && errno == EINTR)
-	{
-		*lnptr = _malloc(2);
-		(*lnptr)[0] = '\n';
-		(*lnptr)[1] = '\n';
-		len = 2;
-		return (len);
-	}
-	if (r < 0)
-		return (-1);
-	if (r == 0 && mode)
-		return (-1);
-	tmp = _realloc(*lnptr, len + r + 4);
-	*size = len + r + 4;
-	*lnptr = tmp;
-	buffer[r] = '\0';
-	_strcat(*lnptr, buffer);
-	len += r;
-	if (!mode)
-		find_n_rep(lnptr, "\n", ";");
-	}
-	if (!mode)
-	{
-	tmp = _realloc(*lnptr, _strlen(*lnptr) + 3);
-	*lnptr = tmp;
-	(*lnptr)[len] = '\n';
-	(*lnptr)[len + 1] = '\0';
-	}
-	return (len);
+    *lnptr = (char *)customMalloc(4);
+    **lnptr = '\0';
+    while (r == BUFFER_SIZE)
+    {
+        if (*lnptr && len > 0)
+            if ((*lnptr)[len - 1] == '\n' && mode)
+                break;
+        r = read(fd, buffer, BUFFER_SIZE);
+        if (r < 0 && errno == EINTR)
+        {
+            **lnptr = '\n',    *(*lnptr + 1) = '\n', len = 2;
+            return (len);
+        }
+        if (r < 0)
+            exit(-1);
+        if (r == 0 && mode)
+            return (-1);
+        tmp = customRealloc(*lnptr, len + r + 4);
+        *size = len + r + 4, *lnptr = tmp;
+        buffer[r] = '\0', customStrcat(*lnptr, buffer), len += r;
+        if (!mode)
+            findAndReplace(lnptr, "\n", ";");
+    }
+    if (!mode)
+    {
+        tmp = customRealloc(*lnptr, customStrlen(*lnptr) + 3);
+        *lnptr = tmp;
+        (*lnptr)[len] = '\n', (*lnptr)[len + 1] = '\0';
+    }
+    return (len);
 }
