a0e00d412d67899bd269b80c93790f62b89b9c06
“LojainHamza” <lojainehab17@gmail.com>
2023-10-19 12:36:41 -0700

    leo

--- str_token.c
+++ str_token.c
@@ -1,7 +1,8 @@
 #include "shell.h"
 
 /**
- * splitString - splits a string into words. Repeat delimiters are ignored
+ * customStringToWords - splits a string into
+ * words. Repeat delimiters are ignored
  * @str: the input string
  * @delimiters: the delimiter string
  * Return: a pointer to an array of strings, or NULL on failure
@@ -12,18 +13,20 @@ char **customStringToWords(char *str, char *delimiters)
 	char **strings;
 
 	if (str == NULL || str[0] == 0)
-		return NULL;
+		return (NULL);
 	if (!delimiters)
 		delimiters = " ";
 	for (i = 0; str[i] != '\0'; i++)
-		if (!isDelimiterCharacter(str[i], delimiters) && (isDelimiterCharacter(str[i + 1], delimiters) || !str[i + 1]))
+		if (!isDelimiterCharacter(str[i],
+					delimiters) && (isDelimiterCharacter(str[i + 1],
+						delimiters) || !str[i + 1]))
 			numWords++;
 
 	if (numWords == 0)
-		return NULL;
+		return (NULL);
 	strings = malloc((1 + numWords) * sizeof(char *));
 	if (!strings)
-		return NULL;
+		return (NULL);
 	for (i = 0, j = 0; j < numWords; j++)
 	{
 		while (isDelimiterCharacter(str[i], delimiters))
@@ -37,18 +40,18 @@ char **customStringToWords(char *str, char *delimiters)
 			for (k = 0; k < j; k++)
 				free(strings[k]);
 			free(strings);
-			return NULL;
+			return (NULL);
 		}
 		for (m = 0; m < k; m++)
 			strings[j][m] = str[i++];
 		strings[j][m] = 0;
 	}
 	strings[j] = NULL;
-	return strings;
+	return (strings);
 }
 
 /**
- * splitString2 - splits a string into words
+ * customStringToWords2 - splits a string into words
  * @str: the input string
  * @delimiter: the delimiter character
  * Return: a pointer to an array of strings, or NULL on failure
@@ -59,16 +62,16 @@ char **customStringToWords2(char *str, char delimiter)
 	char **strings;
 
 	if (str == NULL || str[0] == 0)
-		return NULL;
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		if ((str[i] != delimiter && str[i + 1] == delimiter) ||
 			(str[i] != delimiter && !str[i + 1]) || str[i + 1] == delimiter)
 			numWords++;
 	if (numWords == 0)
-		return NULL;
+		return (NULL);
 	strings = malloc((1 + numWords) * sizeof(char *));
 	if (!strings)
-		return NULL;
+		return (NULL);
 	for (i = 0, j = 0; j < numWords; j++)
 	{
 		while (str[i] == delimiter && str[i] != delimiter)
@@ -82,12 +85,12 @@ char **customStringToWords2(char *str, char delimiter)
 			for (k = 0; k < j; k++)
 				free(strings[k]);
 			free(strings);
-			return NULL;
+			return (NULL);
 		}
 		for (m = 0; m < k; m++)
 			strings[j][m] = str[i++];
 		strings[j][m] = 0;
 	}
 	strings[j] = NULL;
-	return strings;
+	return (strings);
 }
