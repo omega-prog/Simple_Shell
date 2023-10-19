cb138b8b24038cbedbdade7bc8d580871b516597
“LojainHamza” <lojainehab17@gmail.com>
2023-10-19 12:18:23 -0700

    leo

--- modinfo.c
+++ modinfo.c
@@ -6,10 +6,10 @@
  */
 void clearCustomInfo(custom_info_t *info)
 {
-    info->arguments = NULL;
-    info->argumentVector = NULL;
-    info->commandPath = NULL;
-    info->argumentCount = 0;
+	info->arguments = NULL;
+	info->argumentVector = NULL;
+	info->commandPath = NULL;
+	info->argumentCount = 0;
 }
 
 /**
@@ -19,28 +19,28 @@ void clearCustomInfo(custom_info_t *info)
  */
 void setCustomInfo(custom_info_t *info, char **av)
 {
-    int i = 0;
+	int i = 0;
 
-    info->filename = av[0];
-    if (info->arguments)
-    {
-        info->argumentVector = customStringToWords(info->arguments, " \t");
-        if (!info->argumentVector)
-        {
-            info->argumentVector = malloc(sizeof(char *) * 2);
-            if (info->argumentVector)
-            {
-                info->argumentVector[0] = customStringDuplicate(info->arguments);
-                info->argumentVector[1] = NULL;
-            }
-        }
-        for (i = 0; info->argumentVector && info->argumentVector[i]; i++)
-            ;
-        info->argumentCount = i;
+	info->filename = av[0];
+	if (info->arguments)
+	{
+	info->argumentVector = customStringToWords(info->arguments, " \t");
+	if (!info->argumentVector)
+	{
+	info->argumentVector = malloc(sizeof(char *) * 2);
+	if (info->argumentVector)
+	{
+		info->argumentVector[0] = customStringDuplicate(info->arguments);
+		info->argumentVector[1] = NULL;
+	}
+	}
+	for (i = 0; info->argumentVector && info->argumentVector[i]; i++)
+		;
+	info->argumentCount = i;
 
-        replaceCustomAlias(info);
-        replaceCustomVariables(info);
-    }
+	replaceCustomAlias(info);
+	replaceCustomVariables(info);
+	}
 }
 
 /**
@@ -50,24 +50,24 @@ void setCustomInfo(custom_info_t *info, char **av)
  */
 void freeCustomInfo(custom_info_t *info, int all)
 {
-    customFree(info->argumentVector);
-    info->argumentVector = NULL;
-    info->commandPath = NULL;
-    if (all)
-    {
-        if (!info->commandBuffer)
-            free(info->arguments);
-        if (info->environment)
-            freeCustomList(&(info->environment));
-        if (info->history)
-            freeCustomList(&(info->history));
-        if (info->alias)
-            freeCustomList(&(info->alias));
-        customFree(info->custom_environment);
-        info->custom_environment = NULL;
-        customFreeMemory((void **)info->commandBuffer);
-        if (info->readFileDescriptor > 2)
-            close(info->readFileDescriptor);
-        customPutchar(BUFFER_FLUSH);
-    }
+	customFree(info->argumentVector);
+	info->argumentVector = NULL;
+	info->commandPath = NULL;
+	if (all)
+	{
+	if (!info->commandBuffer)
+		free(info->arguments);
+	if (info->environment)
+		freeCustomList(&(info->environment));
+	if (info->history)
+		freeCustomList(&(info->history));
+	if (info->alias)
+		freeCustomList(&(info->alias));
+	customFree(info->custom_environment);
+	info->custom_environment = NULL;
+	customFreeMemory((void **)info->commandBuffer);
+	if (info->readFileDescriptor > 2)
+		close(info->readFileDescriptor);
+	customPutchar(BUFFER_FLUSH);
+	}
 }
